47873aeac107ec4edaf6a05e1784277b5848803b
Graffitio <module.anchor@gmail.com>
2023-10-16 18:38:34 +0900

    Control Block, CPU Simulation

--- cora_cpu/Source/program_address_counter.v
+++ cora_cpu/Source/program_address_counter.v
@@ -24,20 +24,18 @@ module program_address_counter(
     input clk, reset_p,
     input pc_inc, load_pc,
     input pc_rd_en, // pc값이 BUS로 출력될 것인지 말 것인지
-    input [7:0] pc_in,
+    input [7:0] pc_in, // 이동할 데이터의 주소
     output [7:0] pc_out
     );
     
-    // upcounter는 adder와 DFF를 합쳐서 만든다.
+    // upcounter는 adder와 DFF를 합쳐서 만든다.(조합회로와 순차회로의 결합)
+    wire [7:0] sum, next_addr, cur_addr;
+    half_adder_N_bit #(.N(8)) ha8bit(
+.inc(pc_inc), .load_data(cur_addr), .sum(sum));
     
-    always@(posedge clk or posedge reset_p) begin
-        if(reset_p) begin
-            pc_out
-        end
-        else begin
-        
-        end
-    end
+    // 다음 주소로 갈 지, 아니면 아예 다른 주소로 점프할 지 결정
+    assign next_addr = load_pc ? pc_in : sum;
+    
+    register_Nbit_p_alltime #(.N(8)) pc_reg(
+        .d(next_addr), .clk(clk), .reset_p(reset_p), .wr_en(1'b1), .rd_en(pc_rd_en), .register_data(cur_addr), .q(pc_out));
 endmodule
-
-
